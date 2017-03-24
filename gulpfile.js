5255a96ee961ebdc67c57f4a165e4a5ebb0dd21c
alixwang <alixwang@alixwangdeMacBook-Pro.local>
2017-03-24 17:10:12 +0800

    * modified the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload');
 
 gulp.task('watch',function(){
-    var server = livereload();
-
-    gulp.watch('./*.*',function(file){
+    livereload.listen();
+    gulp.watch('./.*',function(file){
+        
         server.changed(file.path);
     });
 })
